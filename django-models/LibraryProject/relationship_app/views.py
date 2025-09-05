e2b4cf89fcfd612e598f80177f09807865f059d7
MonubiJustin <monubijustin@gmail.com>
2025-09-05 13:45:49 +0300

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -45,7 +45,7 @@ def is_member(user):
 
 # Admin view
 @user_passes_test(is_admin)
-def Admin(request):
+def AdminView(request):
     return render(request, "relationship_app/admin_view.html")
 
 # Librarian view
